6ba4c45f605d37557259169194c89c0ac500b884
Ola Kveli <anonexam@yahoo.com>
2018-11-29 20:17:31 +0100

    init

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,7 @@
-# Modify this file to make it your own :-)
-
 variable "app_prefix" {
-  default = "pgr301-zzz"
+  default = "pgr301"
 }
 
 variable "pipeline_name" {
-  default = "pgr301-your-initialsz"
+  default = "exam"
 }
